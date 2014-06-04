36f5b287af59de627900d81cba27e5b0aa507516
Dinouso <pjoder@MacBookPro13PJ.local>
2014-06-04 18:02:38 +0200

    Terrain & Skybox | CameraMovement Tests

---
+++ Assets/Scripts/CamaraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraMovement : MonoBehaviour {
+
+	public float CameraPositionX;
+	public float CameraPositionY;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		CameraPositionX = this.transform.position.x;
+		CameraPositionY = this.transform.position.y;
+
+		Debug.Log (CameraPositionX);
+		Debug.Log (CameraPositionY);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.W)) {
+			this.transform.Translate (new Vector3 (CameraPositionX + 0.01f, 0.0f, CameraPositionY));
+		}
+
+
+	}
+}
